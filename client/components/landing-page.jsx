f50bfb0a7a2d949090d40d9c14cc2572f97eff90
Alec Davidson <alec.davidson@adp.com>
2019-05-04 12:40:38 -0400

    add mobile content

--- client/components/landing-page.jsx
+++ client/components/landing-page.jsx
@@ -146,11 +146,10 @@ class MobileContainer extends Component {
           <Menu.Item as='a' active>
             Home
           </Menu.Item>
-          <Menu.Item as='a'>Work</Menu.Item>
-          <Menu.Item as='a'>Company</Menu.Item>
-          <Menu.Item as='a'>Careers</Menu.Item>
-          <Menu.Item as='a'>Log in</Menu.Item>
-          <Menu.Item as='a'>Sign Up</Menu.Item>
+          <Menu.Item as='a'>Stories</Menu.Item>
+          <Menu.Item as='a'>Map</Menu.Item>
+          <Menu.Item as='a'>About</Menu.Item>
+          <Menu.Item as='a'>Donate</Menu.Item>
         </Sidebar>
 
         <Sidebar.Pusher dimmed={sidebarOpened}>
@@ -167,11 +166,11 @@ class MobileContainer extends Component {
                 </Menu.Item>
                 <Menu.Item position='right'>
                   <Button as='a' inverted>
-                    Log in
+                  Donate
                   </Button>
-                  <Button as='a' inverted style={{ marginLeft: '0.5em' }}>
+                  {/* <Button as='a' inverted style={{ marginLeft: '0.5em' }}>
                     Sign Up
-                  </Button>
+                  </Button> */}
                 </Menu.Item>
               </Menu>
             </Container>
